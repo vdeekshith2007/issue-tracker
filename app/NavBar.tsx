0b62dd0dce14f3f4a9e6f12892ab84b8b3b950f4
vdeekshith2007 <vataparthideekshith18@gmail.com>
2026-09-19 23:29:43 +0530

    add nav bar fixed updated in issue analysis

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,7 +8,7 @@ import classnames from "classnames";
 
 const NavBar = () => {
   const currentPath = usePathname();
-
+  
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
